cf89a5d09d503921bc05ada019330c70401bc7c0
Rocio Florez <rocio.florez@globallogic.com>
2023-03-27 16:14:52 -0300

    fixed ListItem component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useContext } from "react";
 import { DataContext } from "../context/DataContext";
 import { Container, Card, Button } from '@mui/material';
@@ -6,29 +6,34 @@ import { Container, Card, Button } from '@mui/material';
 export const ListItem = () => {
 
   const {data, loading} = useContext(DataContext);
+  const { name } = useParams();
+
+  const filteredData = data.filter(dog => dog.name.toLowerCase() === name.toLowerCase());
 
   return (
     <Container>
       {loading && <p>Loading...</p>}
-      {data.map((item) => (
-        <Card 
-            key={item.id}
-            sx={{ p: 4}}
+      {filteredData.length === 0 ? (
+        <p>Dog not found</p>
+      ) : (
+        <Card
+          key={filteredData[0].id}
+          sx={{ p: 4}}
         >
-          <img src={`https://cdn2.thedogapi.com/images/${item.reference_image_id}.jpg`} alt="" />
-          <h1>{item.name}</h1>
-          {item.description && <p>{item.description}</p>}
+          <img src={`https://cdn2.thedogapi.com/images/${filteredData[0].reference_image_id}.jpg`} alt="" />
+          <h1>{filteredData[0].name}</h1>
+          {filteredData[0].description && <p>{filteredData[0].description}</p>}
           <ul>
-            <li>Height: {item.height.metric} cm</li>
-            <li>Weight: {item.weight.metric} kgs</li>
-            <li>Temperament: {item.temperament}</li>
+            <li>Height: {filteredData[0].height.metric} cm</li>
+            <li>Weight: {filteredData[0].weight.metric} kgs</li>
+            <li>Temperament: {filteredData[0].temperament}</li>
           </ul>
 
           <Link to="/">
             <Button>BACK</Button>
           </Link>
-        </Card>  
-      ))}
+        </Card>
+      )}
     </Container>
   )
 }
